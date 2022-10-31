7993057a45ebc37f0fa844709454b771f26cd822
supahkenneh <chunghlken@gmail.com>
2022-10-31 12:36:59 -0400

    finish geocode api service setup

---
+++ geocodeService.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const GEOCODE_API_URL = 'https://api.mapbox.com/geocoding/v5';
+const GEOCODE_API_SERVICE = 'mapbox.places';
+const { GEOCODE_API_KEY } = process.env;
+
+const getCoordinates = async location => {
+    const queryParams = new URLSearchParams({
+        access_token: GEOCODE_API_KEY
+    });
+
+    return await axios.get(`${GEOCODE_API_URL}/${GEOCODE_API_SERVICE}/${location}.json?${queryParams}`);
+}
+
+module.exports = { getCoordinates }
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const app = express();
+const { getCoordinates } = require('./geocodeService');
 
+const app = express();
 const PORT = process.env.PORT || 8888;
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.get('/coordinates/:address', (req, res) => {
-    console.log(req.params);
-    res.send(req.params.address);
+app.get('/coordinates/:location', async (req, res) => {
+    const responseData = await getCoordinates(req.params.location);
+    if (responseData?.data) res.send(responseData.data);
+    else res.send({ error: true, message: responseData.err })
 })
 
 app.listen(PORT, () => {
